ce984947928bea3e761d581ba56935e3bdc2f873
Kacem Abidi <kacem.abidi02@gmail.com>
2026-02-01 18:50:44 +0100

    Initial Commit

---
+++ CabinKit/CabinKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CabinKitApp.swift
+//  CabinKit
+//
+//  Created by Admin on 1/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct CabinKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
